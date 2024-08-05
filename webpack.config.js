95df0b8cd8f0a307cecc564ae340a17b0a1f2ee2
Mouzone <sunnyliu010@gmail.com>
2024-08-05 11:04:42 -0400

    Change output file to bundle.js and title of  html to Weather App

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         }),
     ],
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
