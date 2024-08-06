24ba6b60b04209f93286aacf2b73c1e19417556e
Mouzone <sunnyliu010@gmail.com>
2024-08-06 11:50:05 -0400

    Center, style elements and properly display correct icon

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import './style.css'
+const icons = require.context('./icons', false, /\.svg$/);
 
 function getDate() {
     const date_element = document.getElementById("date")
@@ -14,7 +15,7 @@ function getLocation() {
 
 async function queryAPI(location, date){
     const API_KEY = "F9FDDA2SDYSNXK4DPT8RRZYXM"
-    const link = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date}?key=${API_KEY}`
+    const link = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date}?unitGroup=us&key=${API_KEY}`
 
     try {
         const response = await fetch(link)
@@ -29,26 +30,26 @@ function updateContent(data) {
     location.textContent = data['resolvedAddress']
 
     const local_time = document.getElementById("local-time")
-    local_time.textContent = data['currentConditions']['datetime']
+    local_time.textContent = `Local Time: ${data['currentConditions']['datetime']}`
 
     const condition = document.getElementById("condition")
     condition.textContent = data['currentConditions']['conditions']
 
-    //todo: retrieve proper icons and set icon element to it
     const icon = document.getElementById("icon")
-    icon.src = `$./src/icons/${data['currentConditions']['icon']}.svg`
+    icon.src = icons(`./${data['currentConditions']['icon']}.svg`)
 
     const humidity = document.getElementById("humidity")
-    humidity.textContent = data['currentConditions']['humidity']
+    humidity.textContent = `Humidity: ${data['currentConditions']['humidity']}%`
 
     const uv_index = document.getElementById("uv-index")
-    uv_index.textContent = data['currentConditions']['uvindex']
+    uv_index.textContent = `UV Index: ${data['currentConditions']['uvindex']}`
 
     const precip_prob = document.getElementById("precip-prob")
-    precip_prob.textContent = data['currentConditions']['precipprob']
+    precip_prob.textContent = `Precip %: ${data['currentConditions']['precipprob']}%`
 
     const precip_amt = document.getElementById("precip-amt")
-    precip_amt.textContent = data['currentConditions']['precip']
+    console.log(data['currentConditions']['precip'])
+    precip_amt.textContent = `Precip Amt: ${data['currentConditions']['precip']}in`
 }
 
 const form = document.querySelector("form")
@@ -60,11 +61,12 @@ form.addEventListener("submit", async event => {
     const data = await queryAPI(location, date)
     updateContent(data)
     form.reset()
-    // todo: reset form upon submission
+    setDefaultDate()
 })
 
 function setDefaultDate() {
     document.getElementById('date').value = new Date().toISOString().split('T')[0]
 }
 
+// todo: on load set it to local location
 setDefaultDate()
